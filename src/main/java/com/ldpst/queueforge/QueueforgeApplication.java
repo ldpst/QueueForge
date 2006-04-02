8e4235959531d35628225124e98ac6e58a710ff2
ldpst <lazydolphins@mail.ru>
2006-04-02 21:25:03 +0300

    feat: add dependencies and db schema

---
+++ src/main/java/com/ldpst/queueforge/QueueforgeApplication.java
@@ -0,0 +1,13 @@
+package com.ldpst.queueforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueforgeApplication.class, args);
+	}
+
+}
